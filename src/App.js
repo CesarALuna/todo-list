3465b8c428f138b2a9ac5a23ee46d1f7b8e9a31e
CesarALuna <CesLun@gmail.com>
2021-10-14 21:47:08 -0700

    initialize components structure

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import './App.css'
+import Main from './components/Main'
+import Header from './components/Header'
+import User from './components/User'
+import AddNewTodo from './components/AddNewTodo'
+import Calendar from './components/Calendar'
+import Projects from './components/Projects'
+import Todos from './components/Todos'
+import EditTodo from './components/EditTodo'
+import AddNewProject from './components/AddNewProject'
+import Next7Days from './components/Next7Days'
 
 function App() {
   return (
     <div className="App">
-      <h1>hello world</h1>
+      <Header>
+        <User />
+        <AddNewTodo />
+        <Calendar />
+        <AddNewProject />
+        <Projects />
+      </Header>
+      <Main>
+        <Todos />
+        <Next7Days />
+        <EditTodo />
+      </Main>
     </div>
   )
 }
---
+++ src/components/Projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import RenameProject from './RenameProject'
+
+function Projects() {
+  return (
+    <div className="Projects">
+      <RenameProject />
+    </div>
+  )
+}
+
+export default Projects
