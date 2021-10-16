6aed7e1700d4d82f9c305e01a5bd71a475c37b2e
CesarALuna <CesLun@gmail.com>
2021-10-16 01:21:45 -0700

    finish Calendar module

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -1,7 +1,30 @@
 import React from 'react'
+import { BsCalendarDate, BsCaretUp } from 'react-icons/bs'
+import { calendarItems } from './constants'
 
 function Calendar() {
-  return <div className="Calendar">Calendar</div>
+  return (
+    <div className="Calendar">
+      <div className="header">
+        <div className="title">
+          <BsCalendarDate size="32" />
+          <p>Calendar</p>
+        </div>
+        <div className="btns">
+          <span>
+            <BsCaretUp size="30" />
+          </span>
+        </div>
+      </div>
+      <div className="items">
+        {calendarItems.map((item) => (
+          <div className="item" key="item">
+            {item}
+          </div>
+        ))}
+      </div>
+    </div>
+  )
 }
 
 export default Calendar
