b3c3c6901b2bbfb7912382baf0efe1c1a9fc8e80
CesarALuna <CesLun@gmail.com>
2021-10-16 17:59:43 -0700

    finish Todos component

--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,7 +1,44 @@
 import React from 'react'
+import Todo from './Todo'
+import Next7Days from './Next7Days'
 
 function Todos() {
-  return <div className="Todos">Todos</div>
+  const selectedProject = 'today'
+  const todos = [
+    {
+      id: 'f24ts4',
+      text: 'feed cats',
+      time: '10:00 AM',
+      date: '10/20/2021',
+      day: '6',
+      checked: false,
+      color: '#000000',
+      project: 'personal',
+    },
+    {
+      id: 'j4j6gn',
+      text: 'go for run',
+      time: '6:00 AM',
+      date: '10/21/2021',
+      day: '1',
+      checked: true,
+      color: '#482841',
+      project: 'work',
+    },
+  ]
+  return (
+    <div className="Todos">
+      <div className="selected-project">{selectedProject}</div>
+
+      <div className="todos">
+        {selectedProject === 'next 7 days' ? (
+          <Next7Days todos={todos} />
+        ) : (
+          todos.map((todo) => <Todo todo={todo} key={todo.id} />)
+        )}
+      </div>
+    </div>
+  )
 }
 
 export default Todos
